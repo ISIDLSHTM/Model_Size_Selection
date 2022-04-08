aefd18abf080577e582d43ca8365e31511de5b7a
ISIDLSHTM <103248759+ISIDLSHTM@users.noreply.github.com>
2022-04-08 13:47:11 +0100

    Update view_scenario.py

--- view_scenario.py
+++ view_scenario.py
@@ -1,1 +1,18 @@
+"""
+Plots the efficacy, toxicity, and utility curve for a given scenario (specified on line 8)
+"""
 
+from scenarios_and_approaches import *
+from crm_classes import *
+
+scenario = scenarioS1 # Change this to visualise a scenario
+
+
+## Run
+print(scenario.calculate_utility_max())
+scenario.plot_toxicity()
+plt.show()
+scenario.plot_efficacy()
+plt.show()
+scenario.plot_utility()
+plt.show()
