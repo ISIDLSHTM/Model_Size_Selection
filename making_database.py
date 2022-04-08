7ba4984d6a35f0337a4e3d7fb6b182ea0309c75d
ISIDLSHTM <103248759+ISIDLSHTM@users.noreply.github.com>
2022-04-08 13:43:36 +0100

    Update making_database.py

--- making_database.py
+++ making_database.py
@@ -1,1 +1,21 @@
+"""
+Creates the experiments table that is used to store simulation results
+"""
 
+import sqlite3
+
+conn = sqlite3.connect('Storing_Database.db') # Creates the file if it doesn't exist
+c = conn.cursor() 
+
+c.execute("""CREATE TABLE experiments (
+            scenario_id str,
+            approach_id str,
+            pred_optimal real,
+            pred_optimal_response real,
+            reached int1,
+            total_utility real,
+            average_utility real
+            )""")
+conn.commit() 
+
+conn.close()
